b000b7e9b501e64c509349645ef5ff4e11b0894e
Darshan <darshansalian53@gmail.com>
2025-01-04 11:13:33 -1200

    Routes Links Tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import './App.css';
+import Nav from './Nav'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <h1>E-Dashboard</h1>
-
-  )
+    <div>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<h1>Product Listing Component</h1>}/>
+          <Route path="/add" element={<h1>Add Listing Component</h1>}/>
+          <Route path="/update" element={<h1>Update Listing Component</h1>}/>
+          <Route path="/logout" element={<h1>Logout Component</h1>}/>
+          <Route path="/profile" element={<h1>Profile Component</h1>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
