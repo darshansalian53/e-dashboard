7d586b0920f678e1a3fcf1c8b4b18f138307bf83
Darshan <darshansalian53@gmail.com>
2025-01-05 10:34:44 -1200

    navbar footer signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 
 import './App.css';
-import Nav from './Nav'
+import Nav from './Components/Nav'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './Components/Footer';
+import SignUp from './Components/SignUp';
 
 function App() {
 
@@ -16,8 +18,10 @@ function App() {
           <Route path="/update" element={<h1>Update Listing Component</h1>}/>
           <Route path="/logout" element={<h1>Logout Component</h1>}/>
           <Route path="/profile" element={<h1>Profile Component</h1>}/>
+          <Route path="/signup" element={<SignUp/>}/>
         </Routes>
       </BrowserRouter>
+      <Footer/>
     </div>
   );
 }
