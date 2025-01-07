536b023aea8daa2ab09f7a86b3a84df39ae93e88
Darshan <darshansalian53@gmail.com>
2025-01-07 09:03:59 -1200

    signup auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-
 import './App.css';
 import Nav from './Components/Navbar/Nav'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './Components/Footer/Footer';
 import SignUp from './Components/SignUp/SignUp';
+import PrivateComponent from './Components/PrivateComponent';
 
 function App() {
 
@@ -13,11 +13,15 @@ function App() {
       <BrowserRouter>
         <Nav />
         <Routes>
+          
+          <Route element={<PrivateComponent />}>
           <Route path="/" element={<h1>Product Listing Component</h1>}/>
           <Route path="/add" element={<h1>Add Listing Component</h1>}/>
           <Route path="/update" element={<h1>Update Listing Component</h1>}/>
           <Route path="/logout" element={<h1>Logout Component</h1>}/>
           <Route path="/profile" element={<h1>Profile Component</h1>}/>
+          </Route>
+
           <Route path="/signup" element={<SignUp/>}/>
         </Routes>
       </BrowserRouter>
--- frontend/src/Components/SignUp/SignUp.jsx
+++ frontend/src/Components/SignUp/SignUp.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+
+import React, { useState , useEffect} from "react";
 import {useNavigate} from 'react-router-dom';
 import './SignUp.css';
 
@@ -7,6 +8,15 @@ const SignUp = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
+
+    
+    useEffect(()=>{
+        const auth= localStorage.getItem('user');
+        if(auth)
+        {
+            navigate('/')
+        }
+    })
 
     const collectData = async () => {
         console.warn(name, email, password)
