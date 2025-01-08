e2814ce0e93e5dbee9a3285107cfb38e0433c171
Darshan <darshansalian53@gmail.com>
2025-01-08 08:48:43 -1200

    .

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,10 +1,33 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import { useNavigate } from "react-router-dom";
 import './Login.css';
 const login = () => {
         const [email, setEmail] = useState("");
         const [password, setPassword] = useState("");
-        const handlelogin=()=>{
-            console.warn(email, password)
+        const navigate = useNavigate();
+        useEffect(()=>{
+            const auth= localStorage.getItem('user');
+            if(auth){
+                navigate("/")
+            }
+        },[])
+        const handlelogin= async()=>{
+            console.warn("email, password", email, password)
+            let result = await fetch('http://localhost:5000/login', {
+                method: 'post',
+                body:JSON.stringify({email,password}),
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            });
+            result = await result.json();
+            console.warn(result)
+            if(result.name){
+                localStorage.setItem("user",JSON.stringify(result));
+                navigate("/")
+            }else{
+                alert("please enter correct details")
+            }
         }
 
 
--- frontend/src/Components/Navbar/Nav.jsx
+++ frontend/src/Components/Navbar/Nav.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import { Link, useNavigate} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Nav.css';
 
 const Nav = () => {
-    const auth= localStorage.getItem('user');
+    const auth = localStorage.getItem('user');
     const navigate = useNavigate
-    const logout =()=>{
+    const logout = () => {
         localStorage.clear();
         navigate('/signup');
     }
 
     return (
         <div>
-            <ul className="nav-ul">
+            {auth ? <ul className="nav-ul">
                 <li><Link to="/">Products</Link></li>
                 <li><Link to="/add">Add Products</Link></li>
                 <li><Link to="/update">Update Products</Link></li>
-
                 <li><Link to="/profile">Profile</Link></li>
-                <li>{ auth ?<Link onClick={logout} to="/signup">Logout</Link>:<Link to="/signup">SignUp</Link>}</li>
-                <li><Link to="/login">Login</Link></li>
+                <li><Link onClick={logout} to="/signup">Logout({JSON.parse(auth).name})</Link></li>:
+
             </ul>
+                :
+                <ul className="nav-ul">
+                    <li><Link to="/signup">SignUp</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                </ul>
+            }
         </div>
 
     )
