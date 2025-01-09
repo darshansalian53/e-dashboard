5a26c7fb6daee752db212815f87c0182bf8c0680
Darshan <darshansalian53@gmail.com>
2025-01-09 08:51:19 -1200

    addproduct api integration

--- frontend/src/Components/AddProduct/AddProduct.jsx
+++ frontend/src/Components/AddProduct/AddProduct.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import './AddProduct.css';
 
 
@@ -7,8 +7,29 @@ const AddProduct = () => {
     const [price, setPrice] = React.useState('');
     const [category, setCategory] = React.useState('');
     const [company, setCompany] = React.useState('');
-    const addProduct=()=>{
+    const [error, setError]= React.useState(false)
+
+
+    const addProduct= async ()=>{
+        console.warn(!name);
+        if(!name || !price || !category || !company)
+        {
+            setError(true)
+            return false;
+        }
+
+
         console.warn(name,price,category,company);
+        const userId = JSON.parse(localStorage.getItem('user'))._id;
+        let result = await fetch("http://localhost:5000/add-product",{
+            method:'post',
+            body:JSON.stringify({name, price, category, company, userId}),
+            headers:{
+                "Content-Type":"application/json"
+            }
+        });
+        result= await result.json();
+        console.warn(result)
     }
 
     return (
@@ -16,16 +37,20 @@ const AddProduct = () => {
             <h1>AddProduct</h1>
             <form action="">
                 <input className="inputbox" type="text" placeholder="Enter product name" 
-                value={name} onChange={(e)=>{setName(e.target.value)}}/>
+                value={name} onChange={(e)=>setName(e.target.value)}/>
+                {error && !name && <span className="invalid-input">Enter valid name</span>}
 
                 <input className="inputbox" type="text" placeholder="Enter product price" 
-                value={price} onChange={(e)=>{setPrice(e.target.value)}}/>
+                value={price} onChange={(e)=>setPrice(e.target.value)}/>
+                {error && !price && <span className="invalid-input">Enter valid price</span>}
 
                 <input className="inputbox" type="text" placeholder="Enter product category" 
-                value={category} onChange={(e)=>{setCategory(e.target.value)}}/>
+                value={category} onChange={(e)=>setCategory(e.target.value)}/>
+                {error && !category && <span className="invalid-input">Enter valid category</span>}
 
                 <input className="inputbox" type="text" placeholder="Enter product company" 
-                value={company} onChange={(e)=>{setCompany(e.target.value)}}/>
+                value={company} onChange={(e)=>setCompany(e.target.value)}/>
+                {error && !company && <span className="invalid-input">Enter valid company</span>}
 
                 <button onClick={addProduct} className="productbutton">AddProduct</button>
             </form>
